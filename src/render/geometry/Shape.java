60320089463f339616b62b148bf647da365ad663
Zetsuki <zetsukiOG@gmail.com>
2025-05-09 12:25:08 +0200

    Added attributes to Shape abstract class (color related attributes)

--- src/render/geometry/Shape.java
+++ src/render/geometry/Shape.java
@@ -1,8 +1,18 @@
 package render.geometry;
 
+import render.utils.ColorRGB;
 import render.utils.Vec3f;
 
 public abstract class Shape {
+    private final ColorRGB color, specular;
+    private final double shininess;
+
+    protected Shape(ColorRGB color, ColorRGB specular, double shininess) {
+        this.color = color;
+        this.specular = specular;
+        this.shininess = shininess;
+    }
+
     public abstract double getIntersection(Vec3f P, Vec3f v);
 
     public Vec3f getIntersectionPoint(Vec3f origin, Vec3f direction, double lambdaI) {
@@ -11,4 +21,15 @@ public abstract class Shape {
 
     public abstract Vec3f getNormal(Vec3f intersectionPoint);
 
+    public ColorRGB getColor() {
+        return color;
+    }
+
+    public ColorRGB getSpecular() {
+        return specular;
+    }
+
+    public double getShininess() {
+        return shininess;
+    }
 }
