3ea381ebdb1acdc88d63e436546937b73cf14284
Zetsuki <zetsukiOG@gmail.com>
2025-05-09 12:45:34 +0200

    Added useful operations on Color

--- src/render/utils/ColorRGB.java
+++ src/render/utils/ColorRGB.java
@@ -3,6 +3,8 @@ package render.utils;
 public class ColorRGB {
     private final float r,g,b;
 
+    public static final ColorRGB black = new ColorRGB(0f,0f,0f);
+
     public ColorRGB(float r, float g, float b) {
         this.r = r;
         this.g = g;
@@ -19,5 +21,17 @@ public class ColorRGB {
 
     public float getB() {
         return b;
+    }
+
+    public ColorRGB multiply(ColorRGB other) {
+        return new ColorRGB(this.r * other.r, this.g * other.g, this.b * other.b);
+    }
+
+    public ColorRGB multiply(double s) {
+        return new ColorRGB(this.r * (float) s, this.g * (float) s, this.b * (float) s);
+    }
+
+    public ColorRGB add(ColorRGB other) {
+        return new ColorRGB(this.r + other.r, this.g + other.g, this.b + other.b);
     }
 }
